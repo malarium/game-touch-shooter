5698e1a5db16480a30a2611df70e2287194b4852
Marcin Gorecki <Marcin.Gorecki@vistex.com>
2020-10-30 14:20:24 +0100

    temp

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("it's on!");
+var leftCannon = document.querySelector('.cannon-left');
+var rightCannon = document.querySelector('.cannon-right');
+
+var handleMovement = function handleMovement(e) {
+  var width = window.innerWidth;
+  var angle = e.screenX / width;
+  rightCannon.style.transform = "rotateZ(".concat(angle * 90, "deg)");
+  leftCannon.style.transform = "rotateY(180deg) rotateZ(".concat((1 - angle) * 90, "deg)");
+};
+
+document.addEventListener('mousemove', handleMovement);
+document.addEventListener('touchmove', handleMovement);
--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log(`it's on!`);
+const leftCannon = document.querySelector('.cannon-left');
+const rightCannon = document.querySelector('.cannon-right');
+const handleMovement = (e) => {
+    const width = window.innerWidth
+    const angle = e.screenX/width;
+    rightCannon.style.transform=`rotateZ(${angle * 90}deg)`;
+    leftCannon.style.transform=`rotateY(180deg) rotateZ(${(1-angle) * 90}deg)`;
+}
+
+document.addEventListener('mousemove', handleMovement)
+document.addEventListener('touchmove', handleMovement)
